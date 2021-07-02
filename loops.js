952b8a57a147f6725a423fac41d88ac77f35bcd9
Bibi <84073518+xBibie@users.noreply.github.com>
2021-07-02 17:38:54 +0200

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,4 +1,30 @@
-const assignGrade = function (score){
+//even-odd-reporter
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 ===0){
+      console.log (i+ 'is even');
+  } else {
+      console.log ('i+ is odd');
+  };
+
+//multiplication-tables
+
+const multiplier = 9;
+for (let i = 0; i <= 10; i++) {
+    let result = multiplier * i;
+    console.log (multiplier + '*' + i + '=' + result);
+  };
+
+// bonus
+for (let multiplier = 0; multiplier <= 10; multiplier++) {
+    for (let i = 0; i <= 10; i++) {
+        let result = multiplier * i;
+        console.log (multiplier + '*' + i + '=' + result);
+    }
+  };
+
+  //grade-assigner
+
+  const assignGrade = function (score){
     if (score >90){
         return 'A';
     } else if (score >80) {
